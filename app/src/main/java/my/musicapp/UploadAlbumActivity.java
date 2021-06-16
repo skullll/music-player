5622b11e39c7442c67043a8789b55276b5338afd
mayur <dingstercr@gmail.com>
2021-06-16 19:07:13 +0530

    online player

--- app/src/main/java/my/musicapp/UploadAlbumActivity.java
+++ app/src/main/java/my/musicapp/UploadAlbumActivity.java
@@ -149,7 +149,7 @@ public class UploadAlbumActivity extends AppCompatActivity implements AdapterVie
             });
         }else{
 
-            Toast.makeText(UploadAlbumActivity.this,"select album image", Toast.LENGTH_SHORT).show();
+            Toast.makeText(UploadAlbumActivity.this,"Select album image", Toast.LENGTH_SHORT).show();
         }
     }
 
